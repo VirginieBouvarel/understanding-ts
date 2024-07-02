44668879191f1b75e7e55c38ba766a80a2c82f65
Virginie <vb@ikimo9.com>
2024-07-02 16:35:52 +0200

    classes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log('Time to get started...');
+class Department {
+  private readonly id: string;
+	public name: string = 'DEFAULT';
+  private employees: string[] = [];
+
+	constructor(id: string, name: string) {
+    this.id = id;
+		this.name = name;
+	}
+  describe() {
+		console.log(`Department (${this.id}): ${this.name}`);
+	}
+  addEmployee(employee: string) {
+    // this.id = 'd2';
+    this.employees.push(employee);
+  }
+  printEmployeesInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department('d1', 'Accounting');
+accounting.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printEmployeesInformation();
+
+// accounting.employees[2] = 'Anna'; // ERROR
+// accounting.printEmployeesInformation();
