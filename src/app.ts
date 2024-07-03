ebe5fb95ec516d71dfad4b233ab8b4d82c6b9d26
Virginie <vb@ikimo9.com>
2024-07-03 10:29:20 +0200

    heritage et polymorphisme

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-  private readonly id: string;
+  protected id: string;
 	public name: string = 'DEFAULT';
   private employees: string[] = [];
 
@@ -20,11 +20,50 @@ class Department {
   }
 }
 
-const accounting = new Department('d1', 'Accounting');
-accounting.describe();
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-accounting.printEmployeesInformation();
+// const accounting = new Department('d1', 'Accounting');
+// accounting.describe();
+// accounting.addEmployee('Max');
+// accounting.addEmployee('Manu');
+// accounting.printEmployeesInformation();
 
 // accounting.employees[2] = 'Anna'; // ERROR
 // accounting.printEmployeesInformation();
+
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT'); // paramètres passés au constructeur de Department
+		this.admins = admins; 
+  }
+
+  describe() {
+		console.log(`Department ${this.name} (${this.id}): ${this.admins.length} admins`);
+	}
+}
+
+const it = new ITDepartment('d1', ['Max']);
+it.describe();
+// it.addEmployee('Max');
+// it.addEmployee('Jil');
+// it.printEmployeesInformation();
+
+// console.log(it);
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const accounting = new AccountingDepartment('d2', []);
+accounting.describe();
+// accounting.addReport('First Report');
+// accounting.addReport('Second Report');
+// accounting.printReports();
